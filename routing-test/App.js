d92915e74e6798cf72e3756e24944a515e6e61ec
Matias Pizarro Pinto <pinkystylee98@gmail.com>
2022-12-04 18:48:24 -0300

    Creacion del proyecto y Login
    Se creó el proyecto con Expo, para luego crear la View de Login (Se esperan cambios).

---
+++ routing-test/App.js
@@ -0,0 +1,83 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View, Image, KeyboardAvoidingView, TouchableOpacity} from 'react-native';
+import { ImageAssets } from './assets/ImageAssets.js';
+
+export default function App() {
+	return (
+    	<KeyboardAvoidingView
+      		behavior= "padding"
+      		style={{flex:1, backgroundColor:'#f3f6f4'}}
+      		enabled
+    	>
+      		<View style={styles.container}>      
+        		<StatusBar style="auto" />
+        		<Image
+          			style={styles.loginLogo}
+          			source={ImageAssets.loginLogo}
+        		></Image>
+				<Text style={styles.title}>Ingresa a Routing</Text>
+				<TextInput 
+					style={styles.input} 
+					placeholder="Email" 
+				></TextInput>
+				<TextInput 
+					style={styles.input} 
+					placeholder="Password" 
+					secureTextEntry
+				></TextInput>
+				<View
+					style={styles.buttonContainer}
+				>
+					<TouchableOpacity style={styles.button}>
+						<Text style={styles.btnText}>Login</Text>
+					</TouchableOpacity>
+				</View>
+				
+
+      		</View>
+    	</KeyboardAvoidingView>
+ 	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#f3f6f4',
+		alignItems: 'center',
+		justifyContent: 'center',
+		padding:24,
+	},
+	title: {
+		fontSize: 30,
+		marginBottom: 15,
+		marginTop: -10,
+		fontWeight: 'bold'
+	},
+	input: {
+		borderColor: '#black',
+		borderWidth: 1,
+		width: '90%',
+		padding: 15,
+		marginBottom: 10,
+		borderRadius: 10,
+	},
+	loginLogo:{
+		width: '100%',
+		height: 400,
+		resizeMode: 'contain'
+	},
+	buttonContainer:{
+		width: '90%',
+		borderRadius: 10,
+		overflow: 'hidden',
+	},
+	button:{
+		width: '100%',
+		padding: 15,
+		backgroundColor: '#2196f3',
+	},
+	btnText: {
+		textAlign: 'center',
+		color: '#FFFFFF'
+	},
+});
---
+++ routing-test/assets/ImageAssets.js
@@ -0,0 +1,3 @@
+export const ImageAssets= {
+    loginLogo: require('./routing-logo.png'),
+};
